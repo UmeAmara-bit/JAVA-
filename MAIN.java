0de1b96874541106bee9501c9aefe5f533c4e0a1
UmeAmara-bit <247119@students.au.edu.pk>
2026-05-05 15:01:43 +0500

    Add files via upload

---
+++ MAIN.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int num1 = 15;
+        int num2 = 25;
+
+        int sum = num1 + num2;
+
+        System.out.println("First Number: " + num1);
+        System.out.println("Second Number: " + num2);
+        System.out.println("Sum is: " + sum);
+    }
+}
